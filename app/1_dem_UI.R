b8d77fdb7c40e0c6474f6d579376fd519c367a22
GodoyTO <tgodoyoliveira@gmail.com>
2026-02-09 21:36:43 -0300

    feat: add population pyramid and ethnicity plots

--- app/1_dem_UI.R
+++ app/1_dem_UI.R
@@ -1,29 +1,55 @@
-
+# Demographic Data Panel ####
 dem_ui <- function(id) {nav_panel(
-    title = "Demografia",
+    title = "Demographic data",
 
+    # Upper cards layer ####
     layout_column_wrap(
         width = 1/3, heights_equal = "row", fill = F,
+        ## Total subjects ####
         value_box(
-            title = "Total subjects",
-            value = textOutput(NS(id, 'all_sub')),
+            title = span("Total subjects",
+                         style = "font-size: 1rem; font-weight: bold;"),
+            value = span(textOutput(NS(id, 'all_sub')), style = "font-size: 2rem;"),
             showcase = span(bs_icon("people-fill"),
                             style = "color: #7b3df1"),
             theme = value_box_theme(bg = "#9c6bf4")
         ),
+        ## Male subjects ####
         value_box(
-            title = "Male subjects",
+            title = span("Male subjects",
+                         style = "font-size: 1rem; font-weight: bold;"),
             value = textOutput(NS(id, 'mal_sub')),
             showcase = span(bs_icon("people-fill"),
                             style = "color: #5f7aa7"),
             theme = value_box_theme(bg = "#6f8ab7")
         ),
+        ## Female subjects ####
         value_box(
-            title = "Female subjects",
+            title = span("Female subjects",
+                         style = "font-size: 1rem; font-weight: bold;"),
             value = textOutput(NS(id, 'fem_sub')),
             showcase = span(bs_icon("people-fill"),
                             style = "color: #a8607a"),
             theme = value_box_theme(bg = "#b8708a")
         )
+    ),
+
+    # Plots layer #####
+    layout_column_wrap(
+        width = 1/2, heights_equal = "row", fill = F,
+        ## Population pyramid #####
+        card(
+            # card_title("Population pyramid", class = "m-0 p-0"),
+            card_body(plotlyOutput(NS(id,'pyr_pop')), class = "m-0 p-0"),
+            full_screen = T
+        ),
+        ## Ethnicity plot layer #####
+        card(
+            # card_title("Ethnicity plot", class = "m-0 p-0"),
+            card_body(plotlyOutput(NS(id,'eth_pop')), class = "m-0 p-0"),
+            card_footer("Categories made for this specific fake dataset"),
+            full_screen = T
+        )
     )
+
 )}
