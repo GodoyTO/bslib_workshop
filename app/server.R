fb8aa19ddebafbc11e2e917f51047520ef49b40b
GodoyTO <47224162+GodoyTO@users.noreply.github.com>
2026-02-09 22:01:55 -0300

    feat: add reactive ds

--- app/server.R
+++ app/server.R
@@ -1,8 +1,16 @@
 # Loading servers
+source("global.R")
 source("1_demSVR.R")
+
 
 ## Calling child servers for item #####
 server <- function(input, output, session) {
+
+    ds <- reactive({
+        data %>%
+            filter(nu_age >= input$age_sli[1],
+                   nu_age <= input$age_sli[2])
+    })
 
     demSVR('dem')
     # second_server('number_2')
