7a5b19a6f40615534e201cc382cdf8236ed97c2c
GodoyTO <47224162+GodoyTO@users.noreply.github.com>
2026-02-18 15:11:56 -0300

    feat: add second navbar server script (diagnostics)

---
+++ app/2_diaSVR.R
@@ -0,0 +1,35 @@
+diaSVR <- function(id) {moduleServer(id,function(input, output, session) {
+
+    ds <- reactive({
+        req(input$age_sli)
+        req(input$dia_sli)
+        data %>%
+            filter(dt_diag >= input$dia_sli[1],
+                   dt_diag <= input$dia_sli[2]) %>%
+            filter(nu_age_diag >= input$age_sli[1],
+                   nu_age_diag <= input$age_sli[2])
+    })
+
+    # Plots layer #####
+    ## Disease stage plot #####
+    output$stg_bar <- renderPlotly({
+
+        df <- ds() %>%
+            select(stage) %>%
+            reframe(n = n(), .by = stage) %>%
+            arrange(stage)
+
+        n <- nrow(ds())
+
+        validate(
+            need(!is.null(df), 'No data'),
+            need(nrow(df) > 0, 'No data')
+        )
+
+        df  %>%  plot_hbar(
+            x = 'n', y = 'stage', color = pal[c(12,8,16,4,24)],
+            total = n, var_text = "Pacients"
+            )
+    })
+
+})}
